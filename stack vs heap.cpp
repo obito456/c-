69db4b5e2a9a6e54cfc2a031a78db22ff9dbac93
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-01-20 19:18:30 +0530

    Create stack vs heap.cpp

---
+++ stack vs heap.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=10;
+    int *ptr=new int();
+    *ptr=20;
+    delete(p);
+    p=new int[4];
+    delete[]p;
+    p=NULL;
+   
+    
+    return 0;
+}
