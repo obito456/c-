47707a10083170a81dbdc7f9dfb6113ec893caa5
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-01-20 17:17:40 +0530

    Create ointers1.cpp
    call by value

---
+++ ointers1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int swap(int a,int b){    //void or int
+    int temp=a;
+    a=b;
+    b=temp;
+}
+
+int main() {
+     int a=10;
+     int b=20;
+     
+     swap(a,b);
+     cout<<a<<" "<<b<<endl;
+    
+    return 0;
+}
