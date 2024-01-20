c1cadce790d706c3177015cb3f300a5be97f7239
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-01-20 19:23:33 +0530

    Create character array.cpp

---
+++ character array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a[100]="vijay";
+    int i=0;
+    
+    while(a[i]!=NULL){
+        cout<<a[i]<<endl;
+        i++;
+    }
+    return 0;
+}
