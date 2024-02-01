5633ddc7df566dd911253519acb1f46678de7776
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-02-01 09:35:15 +0530

    Update vector2.cpp

--- vector2.cpp
+++ vector2.cpp
@@ -11,11 +11,30 @@ int main() {
         cin>>element;
         num.push_back(element);
     }
-    
+    // Output loop: Print the elements of the vector using index
+    cout << "Using index: ";
     for(int i=0; i<num.size(); i++){
         cout<<num[i]<<" ";
     }
     cout<<endl;
+    num.insert(num.begin() + 2, 8);
+    
+    // Output loop: Print the elements of the vector using range-based for loop
+    cout << "Using range-based for loop: ";
+    for(int ele : num) {
+        cout << ele << " ";
+    }
+
+    cout<<endl;
+
+    num.erase(num.end() - 1); // Corrected to remove the last element
+    
+    num.insert(num.begin() + 3, 22);
+    
+    int el = 0;
+    while(el < num.size()) {
+        cout << num[el++] << " ";
+    }
 
     return 0;
 }
