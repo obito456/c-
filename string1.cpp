a5c72f5a9a8f1c3849e8dbae5fbc8805bdbb8171
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-02-01 10:53:51 +0530

    Create string1.cpp

---
+++ string1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    
+    
+    string str0(5, 'n');
+    
+    
+    string str;
+    cin>>str;
+    
+    string str1;
+    getline(cin, str1);
+    
+    cout<<str0<<endl;
+    cout<<str<<endl;
+    cout<<str1<<endl;
+
+    return 0;
+}
