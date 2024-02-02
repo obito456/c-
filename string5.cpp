3804d1bba9a20862a308697917820bb7c966892d
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-02-02 08:14:37 +0530

    Update string5.cpp

--- string5.cpp
+++ string5.cpp
@@ -10,11 +10,12 @@ int main() {
         if (s1[i]>='a' && s1[i]<='z')
             s1[i]-=32;                     //
     }
-// In ASCII, the uppercase letters ('A' to 'Z') have values from 65 to 90, and the lowercase letters ('a' to 'z') have values from 97 to 122. The difference between them is 32.
-// If both the left and right operands of && are true, the result is true.
-// If either the left or right operand (or both) is false, the result is false
     
     cout<<s1<<endl;
     
     return 0;
 }
+
+// In ASCII, the uppercase letters ('A' to 'Z') have values from 65 to 90, and the lowercase letters ('a' to 'z') have values from 97 to 122. The difference between them is 32.
+// If both the left and right operands of && are true, the result is true.
+// If either the left or right operand (or both) is false, the result is false
