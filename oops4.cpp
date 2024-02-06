bee4b1e1c4ee79d2503e9942d6356acb93ac53e8
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-02-06 17:01:03 +0530

    Create oops4.cpp

---
+++ oops4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class cricketer
+{
+public:
+    string name;
+    string role;
+    int age;
+
+    void intro(){
+        cout<<"name="<<name<<endl;
+        cout<<"role="<<role<<endl;
+        cout<<"age="<<age<<endl;
+    }
+
+};
+int main() {
+    
+    cricketer ck1;
+    ck1.name="MSD";
+    ck1.role="batsmen";
+    ck1.age=25;
+    ck1.intro();
+  
+    return 0;
+}
