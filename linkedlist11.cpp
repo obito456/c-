81e3cd1a235e42d5a1bf653bdcc74f3160259a97
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-02-09 00:51:51 +0530

    Create linkedlist11.cpp

---
+++ linkedlist11.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+class node {
+public:
+    int data;
+    node* next;
+
+    node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insert(int val, node*& head) {
+    node* n = new node(val);
+
+    if (head == NULL) {
+        head = n;
+        return;
+    }
+
+    node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node* head) {
+    node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void makecycle(node*& head, int pos) {
+    node* temp = head;
+    node* startnode;
+
+    int count = 1;
+    while (temp->next != NULL) {
+        if (count == pos) {
+            startnode = temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+    temp->next = startnode;
+}
+
+bool detectcycle(node* head) {
+    node* slow = head;
+    node* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (fast == slow) {
+            return true;
+        }
+    }
+    return false;
+}
+void removecycle(node* &head){
+    node*slow=head;
+    node* fast=head;
+
+    do{
+        slow=slow->next;
+        fast=fast->next->next;
+
+    }while(slow!=fast);
+    fast=head;
+    while(slow->next!=fast->next){
+        slow=slow->next;
+        fast=fast->next;
+    }
+    slow->next=NULL;
+}
+
+int main() {
+    node* head = NULL;
+
+    insert(1, head);
+    insert(2, head);
+    insert(3, head);
+    insert(4, head);
+    insert(5, head);
+    insert(6, head);
+
+    makecycle(head, 3);
+
+    // Uncomment the line below to display the linked list (commented for clarity)
+    //display(head);
+    cout <<detectcycle(head)<< endl;
+    removecycle(head);
+    cout <<detectcycle(head)<< endl;
+    display(head);
+
+    return 0;
+}
