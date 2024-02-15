7994b8a1530b4c939c466bca2bf217acab3e9530
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-02-15 09:13:40 +0530

    Create number system v.cpp

---
+++ number system v.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a=20;
+    int b=024;
+    int c=0x14;
+    int d=0b10100;
+    
+    cout<<"a="<<a<<"\n";
+    cout<<"b="<<b<<"\n";
+    cout<<"c="<<c<<"\n";
+    cout<<"d="<<d<<"\n";
+ 
+  return 0;
+}
