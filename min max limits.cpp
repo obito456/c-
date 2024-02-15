5c5d5f1a63946dd1a4e2f9dcdf2da7fb7aacd0e6
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-02-15 09:24:25 +0530

    Create min max limits.cpp

---
+++ min max limits.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <climits>  // Include for limits on data types
+
+int main() {
+    int minValue = INT_MIN;  // Minimum value for int
+    int maxValue = INT_MAX;  // Maximum value for int
+
+    std::cout << "Minimum value for int: " << minValue << std::endl;
+    std::cout << "Maximum value for int: " << maxValue << std::endl;
+
+    return 0;
+}
