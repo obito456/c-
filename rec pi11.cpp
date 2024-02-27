8b86d21ee34ee27022c29f088c680d82ee78dd09
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-02-27 21:14:28 +0530

    Update rec pi11.cpp

--- rec pi11.cpp
+++ rec pi11.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 void replacepi(string s) {
-    if (s.length() == 0) {
+    if (s.length() == 0) {            // Base case: If the length of the string is 0, do nothing and return
         return;
     }
 
-    if (s[0] == 'p' && s[1] == 'i') {
-        cout << "3.14";
+    if (s[0] == 'p' && s[1] == 'i') {           // Check if the first two characters are "pi"
+        cout << "3.14";                         // Replace "pi" with "3.14" and make a recursive call with the rest of the string
         replacepi(s.substr(2));
     } else {
-        cout << s[0];
+        cout << s[0];                          // If the first two characters are not "pi", print the current character and make a recursive call
         replacepi(s.substr(1));
     }
 }
