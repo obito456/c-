123fd91820efa76d0084cb433e007f854cea05ad
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-14 12:14:26 +0530

    Create REV.cpp

---
+++ REV.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n=4567;
+    int rev=0;
+
+    while(n>0){
+        int last=n%10;
+        rev=rev*10+last;
+        n=n/10;
+    }
+
+    cout<<rev<<endl;
+
+    
+    return 0;
+}
