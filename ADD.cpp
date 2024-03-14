6b6c733d75d937b49845beac739f5d315a688364
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-14 12:37:00 +0530

    Create ADD.cpp

---
+++ ADD.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int add(int x,int y){
+    int res=x+y;
+    return res;
+}
+
+int main(){
+
+    int a=10;
+    int b=20;
+    cout<<add(a,b)<<endl;
+
+    return 0;
+}
