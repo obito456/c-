e0be332fa1d846a495a3b7712b90e7f1c45ad408
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-26 11:14:45 +0530

    Create Int  01 .cpp

---
+++ Int  01 .cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=15;
+    signed b=200;
+    unsigned c=-250;
+    signed int d=-300;
+    unsigned int e=350;
+
+    cout<<"a size="<<sizeof(a)<<endl;
+    cout<<"b size="<<sizeof(b)<<endl;
+    cout<<"c size="<<sizeof(c)<<endl;
+    cout<<"d size="<<sizeof(d)<<endl;
+    cout<<"e size="<<sizeof(e)<<endl;
+
+    return 0;
+}
