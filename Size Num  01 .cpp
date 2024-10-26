4b4f6db4a91185b44961e2ca41c2cf95d09be0f9
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-26 11:58:11 +0530

    Create Size Num  01 .cpp

---
+++ Size Num  01 .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout<<"short size=     "<<sizeof(short)<<endl;
+    cout<<"int size=       "<<sizeof(int)<<endl;
+    cout<<"float size=     "<<sizeof(float)<<endl;
+    cout<<"long size=      "<<sizeof(long)<<endl;
+    cout<<"double size=    "<<sizeof(double)<<endl;
+    cout<<"signed size=    "<<sizeof(signed)<<endl;
+    cout<<"unsigned size=  "<<sizeof(unsigned)<<endl;
+    cout<<"long long size= "<<sizeof(long long)<<endl;
+
+    return 0;
+}
