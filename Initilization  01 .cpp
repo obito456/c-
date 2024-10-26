c1bfc5695704b40b519f582f11967e1a2d0c246d
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-26 11:03:13 +0530

    Update Initilization  01 .cpp

--- Initilization  01 .cpp
+++ Initilization  01 .cpp
@@ -6,11 +6,13 @@ int main() {
     int b {017};
     int c {a+b};
     int d {};
+    int e;
 
     cout<<"a="<<a<<endl;
     cout<<"b="<<b<<endl;
     cout<<"c="<<c<<endl;
     cout<<"d="<<d<<endl;
+    cout<<"e="<<e<<endl;
 
     return 0;
 }
