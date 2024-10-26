3916c2a7c4ee327dadf043fd035cc9b769361f8b
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-26 11:53:04 +0530

    Create FDL  01 .cpp

---
+++ FDL  01 .cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a=15.3334444666677777f;
+    double b=2.00111122233334444d;
+    long double c=-2.50333344455667778L;
+
+    cout<<"a size="<<sizeof(a)<<endl;
+    cout<<"b size="<<sizeof(b)<<endl;
+    cout<<"c size="<<sizeof(c)<<endl;
+
+    return 0;
+}
