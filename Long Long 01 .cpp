63cf2ff44d4d4ab36c51e2ef2f977b62c6144466
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-26 11:49:20 +0530

    Create Long Long 01 .cpp

---
+++ Long Long 01 .cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long a=15;
+    long long int b=200;
+    signed long long c=-250;
+    signed long long int d=-300;
+    unsigned long long int e=350;
+
+    cout<<"a size="<<sizeof(a)<<endl;
+    cout<<"b size="<<sizeof(b)<<endl;
+    cout<<"c size="<<sizeof(c)<<endl;
+    cout<<"d size="<<sizeof(d)<<endl;
+    cout<<"e size="<<sizeof(e)<<endl;
+
+    return 0;
+}
