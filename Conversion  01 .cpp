452ec1f6f2849fe83abd25786ff0d9525c2d8aef
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-26 10:52:49 +0530

    Create Conversion  01 .cpp

---
+++ Conversion  01 .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=15;
+    int b=017;
+    int c=0x0F;
+    int d=0b00001111;
+
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    cout<<"c="<<c<<endl;
+    cout<<"d="<<d<<endl;
+
+    return 0;
+}
