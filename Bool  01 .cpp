54a74cdda3de200e5df1c18d922d75d36b179f5c
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-26 12:06:25 +0530

    Create Bool  01 .cpp

---
+++ Bool  01 .cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    bool a=true;
+    bool b=false;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<boolalpha;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    return 0;
+}
