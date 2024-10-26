4adcb93bcdd908777f557902d1f787ab4a27d8cd
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-26 11:01:22 +0530

    Create Initilization  01 .cpp

---
+++ Initilization  01 .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=15;
+    int b {017};
+    int c {a+b};
+    int d {};
+
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    cout<<"c="<<c<<endl;
+    cout<<"d="<<d<<endl;
+
+    return 0;
+}
